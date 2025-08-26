44587962e9f872a83d48a97dc4a19f198895776a
Marvin Rios <ia.marvinrios@ufg.edu.sv>
2025-08-26 11:14:34 -0600

    🔨Fix: Organize some files.

---
+++ Assets/Scripts/movimiento.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private Vector2 direccion;
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + direccion * velocidadMovimiento * Time.fixedDeltaTime);
+    } 
+}
